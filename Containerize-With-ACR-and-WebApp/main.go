915f6096ddf95e670913b5b8554e7af509648be5
Rafael Martinez <rgmllc@yahoo.com>
2025-07-29 12:17:13 -0400

    Add Go container project for Azure Web App deployment

---
+++ Containerize-With-ACR-and-WebApp/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from Go Web App! Brought to you by DevOps Raf")
+	})
+
+	port := os.Getenv("PORT") // Azure provides PORT as an env variable
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
